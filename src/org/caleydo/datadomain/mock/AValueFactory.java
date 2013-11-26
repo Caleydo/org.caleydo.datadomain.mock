88c2105b34a8ff3f17b68e766da17ec3427f7e33
Samuel Gratzl <samuel_gratzl@gmx.at>
2013-11-26 19:57:46 +0100

    improved generation

--- src/org/caleydo/datadomain/mock/AValueFactory.java
+++ src/org/caleydo/datadomain/mock/AValueFactory.java
@@ -5,6 +5,10 @@
  *******************************************************************************/
 package org.caleydo.datadomain.mock;
 
+import org.caleydo.core.data.collection.EDimension;
+import org.caleydo.core.util.color.Color;
+import org.caleydo.core.util.color.ColorBrewer;
+
 /**
  * @author Samuel Gratzl
  *
@@ -24,5 +28,29 @@ public abstract class AValueFactory {
 
 	public double getCenter() {
 		return 0;
+	}
+
+	public String getIDType(EDimension dim) {
+		return "mock_" + dim.name().toLowerCase();
+	}
+
+	public String getIDCategory(EDimension dim) {
+		return "mock_" + dim.name().toLowerCase();
+	}
+
+	/**
+	 * @return
+	 */
+	public Color nextColor() {
+		ColorBrewer b = ColorBrewer.Set3;
+		int max = b.getSizes().last();
+		return b.get(max, nextInt(max));
+	}
+
+	/**
+	 * @return
+	 */
+	public String nextString() {
+		return String.valueOf(nextInt(100));
 	}
 }
